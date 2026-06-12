1093a0315ae27132bcf5cfa8a30190701b499edd
Mandem o nick de voces <patztortinha@gmail.com>
2026-06-11 22:37:46 -0300

    Add files via upload
    dicionario com D_fus=5,61; L_fus=66,82..
    achei mais fácil upar tudo em paralelo pra poder rodar sem precisar ficar substituindo coisa.

---
+++ o outro layout/aula02/run_geometry.py
@@ -0,0 +1,35 @@
+'''
+This script executes the Fokker 100 example
+'''
+
+#IMPORTS
+import numpy as np
+from designTool.standard_airplane import standard_airplane
+from designTool.geometry import geometry
+from designTool.plots import plot_geometry
+import pprint
+
+#=========================================
+
+# SETUP
+
+# Select airplane name from the standard_airplane function in designTool
+#airplane_name = 'fokker100'
+airplane_name = 'my_airplane'
+
+#=========================================
+
+# EXECUTION
+
+# Load the airplane dictionary
+airplane = standard_airplane(airplane_name)
+
+# Execute the geometry module to compute all dimensions.
+# This updates the airplane dictionary with new entries.
+geometry(airplane)
+
+# Plot airplane
+plot_geometry(airplane, figname='3dview.png', az1=45, az2=-135)
+
+# Print final dictionary
+print(pprint.pformat(airplane))
