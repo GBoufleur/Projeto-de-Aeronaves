d3442104ea1aceaa5cd6b0e4a558d88ef7d1c683
pigattojp <jppigatto22@hotmail.com>
2026-08-18 21:50:10 -0300

    Mudanças feitas no dia do churrasco de fim da primeira fase.Não lembro o que era, estava bêbado

--- run_geometry.py
+++ run_geometry.py
@@ -16,7 +16,7 @@ from designTool.plot_views import plot_geometry_views
 # SETUP
 
 # Select airplane name from the standard_airplane function in designTool
-airplane_name = 'crusair2'
+airplane_name = 'crusair1'
 #airplane_name = 'my_airplane'
 
 #=========================================
@@ -33,7 +33,7 @@ geometry(airplane)
 # Plot airplane
 plot_geometry(airplane, figname='3dview.png', az1=45, az2=-135)
 
-# Plot airplane from three standard views
+#Plot airplane from three standard views
 plot_geometry(airplane, figname='3dview_bottom.png', az1=-90, az2=0)
 plot_geometry(airplane, figname='3dview_front.png', az1=0, az2=-90)
 plot_geometry(airplane, figname='3dview_side.png', az1=0, az2=0)
